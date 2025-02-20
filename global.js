cebe0568f1c5ffaa901bfd6f59ce8c02151898e1
Emilia Charno <ercharno@mit.edu>
2025-02-20 15:53:38 -0500

    comment out step 2

--- global.js
+++ global.js
@@ -5,17 +5,17 @@ function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
 
-// STEP 2
-// 2.1
-let navLinks = $$("nav a");
+// // STEP 2
+// // 2.1
+// let navLinks = $$("nav a");
 
-// 2.2
-let currentLink = "TODO: FILL IN!!"
+// // 2.2
+// let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname)
 
-// 2.3
-if (currentLink) { // or if (currentLink !== undefined)
-	// TODO: FILL IN!!
-}
+// // 2.3
+// if (currentLink) { // or if (currentLink !== undefined)
+// 	currentLink.classList.add("current");
+// }
 
 // STEP 3
 // TODO: Remove <nav> menu from all HTML pages!
